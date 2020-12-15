c45424fd9e2c13ccb7fb4769954601d9aaf6ca4a
dvranga <dvranga@users.noreply.github.com>
2020-12-15 06:51:35 +0530

    [ranganath]Added:Created logback.xml file to add logger to log the messages using the simple logging framework SLF4J

--- src/main/java/com/example/demo/HelloworldApplication.java
+++ src/main/java/com/example/demo/HelloworldApplication.java
@@ -1,20 +1,28 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+//import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.example.demo.component.DemoBean;
+import com.example.demo.controller.HelloRestController;
 
 
 @SpringBootApplication
 public class HelloworldApplication {
+	
+		public static Logger logger=LoggerFactory.getLogger(HelloworldApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		logger.debug("Hello world");
 		ApplicationContext context=SpringApplication.run(HelloworldApplication.class, args);
 		DemoBean demoBean=context.getBean(DemoBean.class);
-		System.out.println("demoBean  "+demoBean.toString());
+		logger.debug("demoBean  "+demoBean.toString());
+		HelloRestController controller=context.getBean(HelloRestController.class);
+		logger.debug(controller.toString());
 	}
 
 }
