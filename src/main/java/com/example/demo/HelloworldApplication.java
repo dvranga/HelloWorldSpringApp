e3e924802151b88f7bf128ee5b4abaf2b8455537
dvranga <dvranga@users.noreply.github.com>
2020-12-15 06:05:12 +0530

    [ranganath]Added:Created a project to learn spring concepts

--- src/main/java/com/example/demo/HelloworldApplication.java
+++ src/main/java/com/example/demo/HelloworldApplication.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.component.DemoBean;
+
 
 @SpringBootApplication
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		SpringApplication.run(HelloworldApplication.class, args);
+		ApplicationContext context=SpringApplication.run(HelloworldApplication.class, args);
+		DemoBean demoBean=context.getBean(DemoBean.class);
+		System.out.println("demoBean  "+demoBean.toString());
 	}
 
 }
